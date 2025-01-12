a42b649c618f1a0b5e155886af0cb02cb5c805b3
Lucas <lucasreiste@gmail.com>
2025-01-11 23:31:05 -0300

    feat: add rented books listing page and component

---
+++ pages/my-books.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <Header />
+
+    <div class="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-7xl mx-auto">
+        <h2 class="text-3xl font-bold mb-8 text-center">Livros Alugados</h2>
+
+        <div v-if="rentedBooks.length === 0" class="text-center">
+          <p class="text-xl text-gray-600">Nenhum livro alugado.</p>
+        </div>
+
+        <div v-else>
+          <RentedBookList
+            :books="rentedBooks"
+            @some-action="handleSomeAction"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Header from "@/components/Header/Header.vue";
+import RentedBookList from "@/components/Books/RentedBookList/RentedBookList.vue";
+import type { Book } from "@/types/book";
+
+const rentedBooks = ref<
+  Array<
+    Book & {
+      paymentMethod?: string;
+      returnDate?: string;
+    }
+  >
+>([]);
+
+onMounted(() => {
+  const rentalInfo = localStorage.getItem("rentalInfo");
+  if (rentalInfo) {
+    const parsedInfo = JSON.parse(rentalInfo);
+
+    rentedBooks.value = [
+      {
+        ...parsedInfo.book,
+        paymentMethod: parsedInfo.paymentMethod,
+        returnDate: parsedInfo.returnDate,
+      },
+    ];
+  }
+});
+
+const handleSomeAction = (book: Book) => {
+  alert(`Ação no livro: ${book.title}`);
+};
+</script>
